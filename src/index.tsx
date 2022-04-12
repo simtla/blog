5a05bf12e0aab60b0c570374a73c251d2a07d11f
Jin Woo <help7577@gmail.com>
2022-04-12 23:10:13 +0900

    react-dom 17 -> 18 버전 업 및 createRoot로 변경

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
+const rootNode = document.getElementById("root");
+
+if (!rootNode) throw new Error("Failed to find the root element");
+
+const root = createRoot(rootNode);
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
